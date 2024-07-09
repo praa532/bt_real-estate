893edc874b2c57535fe07827bd6adfae6bd135c1
praa532 <prashantprasad135@gmail.com>
2024-07-09 11:56:20 +0530

    setting frontend

--- btre_project/settings.py
+++ btre_project/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#Manually Added
+
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index2.html')
+    return render(request, 'pages/index.html')
+    
+def about(request):
+    return render(request, 'pages/about.html')
